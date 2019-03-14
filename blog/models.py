c53ba80f50ca4391fdfdc40b72f37d2691db0e43
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-14 21:18:30 +0530

    Changed PostgreSQL in settings.py and made models in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 # Create your models here.
+class Blog(models.Model):
+    blog_id=models.CharField(max_length=1000000,unique=True)
+    heading=models.CharField(max_length=100)
+    content=models.CharField(max_length=1000000)
+    post_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post_id
--- dbms/settings.py
+++ dbms/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'dbms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
