c5f4901bb400a8bd36eb596d13503ed2f71ebd41
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-15 23:20:35 +0530

    Blog Running

--- blog/models.py
+++ blog/models.py
@@ -8,23 +8,23 @@ class interest(models.Model):
     content=RichTextUploadingField()
 
 class Blog(models.Model):
+    author= models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
     heading=models.CharField(max_length=200)
     content=RichTextUploadingField()
     post_date=models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
     upvotes=models.IntegerField(default=0)
-    interests=models.ManyToManyField(interest)
+    interests=models.ManyToManyField(interest,null=True,blank=True)
     def __str__(self):
         return str(self.id)
 
 class Comment(models.Model):
     blog_id=models.ForeignKey(Blog,on_delete=models.CASCADE)
     content = RichTextUploadingField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author= models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
     comment_date=models.DateTimeField(auto_now_add=True)
     upvotes=models.IntegerField(default=0)
 
 
     def __str__(self):
 
-       return self.Comment_id
+       return str(self.id)
