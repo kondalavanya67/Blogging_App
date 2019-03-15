cf8b0150f641e2810f8e8f05ab7955a75e8afb84
bradison <ssbharadwaja1999@gmail.com>
2019-03-15 21:39:51 +0530

    added forms and blog using ckeditor

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
+from .forms import story
 # Create your views here.
-
+from django.contrib.auth.models import User
 def blog_display(request):
     if request.method == 'POST':
-        pass
+        form = story(request.POST or None,instance=User.objects.get(username='shit'))
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Your story has been posted')
+
     else:
-        return render(request,'blog/form.html',{})
+        context = {'form':story()}
+        return render(request,'blog/form.html',context)
 
 
 def create_story(request):
