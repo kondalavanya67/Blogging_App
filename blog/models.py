f780f983f8e7282242fe494272efe5685a70438d
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-15 19:19:44 +0530

    Models for comment

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,15 @@ class Blog(models.Model):
     heading=models.CharField(max_length=100)
     content=models.CharField(max_length=1000000)
     post_date=models.DateTimeField(auto_now_add=True)
+    upvotes=models.InigerField(default=0)
+    def __str__(self):
+        return self.Blog_id
+
+class Comment(models.Model):
+    blog_id=models.ForeignKey(Blog,on_delete=models.CASCADE)
+    content=models.CharField(max_length=100)
+    comment_date=models.DateTimeField(auto_now_add=True)
+    upvotes=models.InigerField(default=0)
 
     def __str__(self):
-        return self.post_id
+        return self.Comment_id
