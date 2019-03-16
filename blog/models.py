35be137e802813512faf01fa4f60bfe1368bb437
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-16 13:24:19 +0530

    Blog making, viewing and comments working sychronously

--- blog/models.py
+++ blog/models.py
@@ -8,19 +8,20 @@ class interest(models.Model):
     content=RichTextUploadingField()
 
 class Blog(models.Model):
-    author= models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+    author= models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     heading=models.CharField(max_length=200)
     content=RichTextUploadingField()
     post_date=models.DateTimeField(auto_now_add=True)
     upvotes=models.IntegerField(default=0)
     interests=models.ManyToManyField(interest,null=True,blank=True)
+
     def __str__(self):
         return str(self.id)
 
 class Comment(models.Model):
     blog_id=models.ForeignKey(Blog,on_delete=models.CASCADE)
     content = RichTextUploadingField()
-    author= models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+    author= models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     comment_date=models.DateTimeField(auto_now_add=True)
     upvotes=models.IntegerField(default=0)
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
-from .forms import story
-from .models import Blog
+from .forms import story, comment
+from .models import Blog, Comment
 # Create your views here.
 from django.contrib.auth.models import User
 def blog_display(request):
@@ -18,6 +18,20 @@ def blog_display(request):
         context = {'form':story()}
         return render(request,'blog/form.html',context)
 
+def home(request):
+    user=request.user
+    data=Blog.objects.all()
+    return render(request, 'home.html',{'data':data})
 
-def create_story(request):
-    return HttpResponse('Create Story HomePage')
+def show_blog(request, blog_id):
+    user=request.user
+    blog = get_object_or_404(Blog, pk=blog_id)
+    comments=Comment.objects.filter(blog_id=blog)
+    if request.method == 'POST':
+        form = comment(request.POST or None)
+        if form.is_valid():
+        
+            Comment.objects.create(blog_id=blog,author=user,content=form.cleaned_data['content'])
+            comments=Comment.objects.filter(blog_id=blog)
+            return render(request,'blog.html',{'blog':blog , 'comments':comments,'form':comment()})
+    return render(request, 'blog.html', {'blog': blog, 'comments':comments,'form':comment()})
