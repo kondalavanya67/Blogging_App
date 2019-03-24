2ad12cc72a9f55ae313c9175138f04b6ddb139d5
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-24 12:14:25 +0530

    Added self to interest model

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,9 @@ class interest(models.Model):
     base_image=models.ImageField(upload_to='media/interest/')
     related_topics=models.ManyToManyField(interest, blank=True)
 
+    def __str__(self):
+        return str(self.interest_name)
+
 class Blog(models.Model):
     author= models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     heading=models.CharField(max_length=200)
