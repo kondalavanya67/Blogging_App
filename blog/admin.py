e700a9cccccf79fe8544a769d12a1007eb45577a
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-03-28 19:14:11 +0530

    Removed Some Errors in models and upvotes working with Ajax

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog,Comment
+from .models import Blog,Comment, interest
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Comment)
