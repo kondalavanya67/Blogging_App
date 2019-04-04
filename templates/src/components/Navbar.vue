d6ee41a15747b52032614f0b155f7926c6901bed
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-04-04 16:36:52 +0530

    added frontend

---
+++ templates/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <nav>
+            <v-toolbar flat app color="white">
+                <v-toolbar-title>
+                <span class="font-weight-bold text-uppercase">
+                    OASP
+                </span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-text-field
+                        hide-details
+                        prepend-icon="search"
+                        single-line
+                ></v-text-field>
+                <v-btn flat color="black">Sign Up</v-btn>
+                <v-btn flat color="black">Log In</v-btn>
+            </v-toolbar>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Navbar',
+    data(){
+        return{
+        }
+    },
+
+    methods: {
+    }
+    
+}
+</script>
+
+<style scoped> 
+
+</style>
+
---
+++ templates/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import {store} from './store'
+import './registerServiceWorker'
+
+var VueTruncate = require('vue-truncate-filter')
+Vue.use(VueTruncate)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
