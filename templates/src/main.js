c64a2e4c3d4a96ed1104bb06e19fc061d025c900
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-04-06 22:01:53 +0530

    modified src/main.js

--- templates/src/main.js
+++ templates/src/main.js
@@ -3,10 +3,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import {store} from './store'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 import './registerServiceWorker'
 
 var VueTruncate = require('vue-truncate-filter')
+
 Vue.use(VueTruncate)
+Vue.use( CKEditor );
 
 Vue.config.productionTip = false
 
