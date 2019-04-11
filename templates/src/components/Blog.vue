ae426810604655a18f618d41da164defb337e4a1
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-04-11 12:30:11 +0530

    modified Blog.vue

--- templates/src/components/Blog.vue
+++ templates/src/components/Blog.vue
@@ -3,12 +3,10 @@
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
       <template v-slot:activator="{ on }">
         
-            
             <v-btn flat outline color="primary" v-on="on" class="mx-2">
                 Create Post
             </v-btn>
             
-        
       </template>
       
       <v-card>
@@ -23,23 +21,61 @@
         <v-btn outline color="primary" flat @click="dialog = false">Post</v-btn>
         
         </v-toolbar>
-        <v-container class="">
-        <v-layout row wrap class="">
+
+        <v-container class="px-5">
+
+        <v-layout row wrap>
             <v-flex xs12 sm6 md12 lg12>
                 <v-flex xs12 sm6 md6 lg6>
-                    <v-text-field
-                    label="Title"
-                ></v-text-field>
+                   <input type="text" id="title" placeholder="Untitled Post" class="title">
                 </v-flex>
             </v-flex>
+            <v-layout row>
+                <v-dialog v-model="file"  width="600px">
+                    <template v-slot:activator="{ on }">
+                        <v-flex class="my-4 picture-box py-5" xs12 sm12 md12 lg12 v-on="on">
+                            <p class="text-xs-center"><v-icon>camera_alt</v-icon>    Set Cover Photo ( displays as 760 x 300 )</p>
+                        </v-flex>
+                    </template>
+                    <v-card>
+                          <v-navigation-drawer
+                            floating
+                            permanent
+                            stateless
+                            value="true"
+                            >
+                            <v-list dense>
+                            <v-list-tile
+                                v-for="item in items"
+                                :key="item.title"
+                            >
+                                <v-list-tile-action>
+                                <v-icon>{{ item.icon }}</v-icon>
+                                </v-list-tile-action>
+
+                                <v-list-tile-content>
+                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            </v-list>
+                        </v-navigation-drawer>
+                        <v-flex>
+                            
+                        </v-flex>
+                    </v-card>
+                </v-dialog>
+            </v-layout>
             <v-flex lg12 md12 sm12 xs12 class="py-3">
                 <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
             </v-flex>
         </v-layout>
-    </v-container>
+
+        </v-container>
+   
       </v-card>
     </v-dialog>
-  
+         
+
 </template>
 
 
@@ -50,6 +86,14 @@ export default {
     name:'Blog',
     data:()=>({
         dialog: false,
+        file:false,
+
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        right: null,
+
         notifications: false,
         sound: true,
         widgets: false,
@@ -61,3 +105,18 @@ export default {
     })
 }
 </script>
+
+
+<style lang="css">
+    .picture-box{
+        border-style: dotted;
+        height: 130px;
+        cursor: pointer;
+    }
+    .title{
+        font-size: 2.7rem!important;
+        height:50px;
+        border:none;
+        outline:none;
+    }
+</style>
